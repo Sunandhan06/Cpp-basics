b7a977715720ea10b52e28d959995531b85988bf
Sunandhan06 <79313170+Sunandhan06@users.noreply.github.com>
2021-02-22 14:40:18 +0530

    basics_created

---
+++ basics.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+ cout << "I love rockets !\n";
+
+ cout << "Drawing time !!!!!!!\n";
+
+ cout << "          @\n";
+ cout << "         @ @\n";
+ cout << "        @   @\n";
+ cout << "       @     @\n";
+ cout << "      @       @\n";
+ cout << "      @@@@@@@@@\n";
+ cout << "      $       $\n";
+ cout << "      $       $\n";
+ cout << "      $       $\n";
+ cout << "      $       $\n";
+ cout << "      $       $\n";
+ cout << "      $       $\n";
+ cout << "      $       $\n";
+ cout << "      $       $\n";
+ cout << "      $       $\n";
+ cout << "      $       $\n";
+ cout << "      $       $\n";
+ cout << "      $$$$$$$$$\n";
+ cout << "      @       @\n";
+ cout << "     @         @\n";
+ cout << "    @           @\n";
+ cout << "    @           @\n";
+ cout << "     @         @\n";
+ cout << "      @       @\n";
+ cout << "       @     @\n";
+ cout << "        @   @\n";
+ cout << "         @ @\n";
+ cout << "          @\n";
+
+
+
+   cout << 2 << endl;
+    cout << 3.242 << endl;
+     cout << 4 + 5 << endl;
+      cout << 3.4424 + 424.242 << endl;
+       cout << 2*3 << endl;
+        cout << 4/3 << endl;
+         cout << 4.0/3.0 << endl;
+          cout << 5 % 4 << endl; // for getting remainder
+
+int a = 5;
+
+a++;
+
+cout << a << endl;
+
+a--;
+
+cout << a << endl;
+
+a += 5;
+
+cout << a << endl;
+
+a -= 5;
+
+cout << a << endl;
+
+a *= 2;
+
+cout << a << endl;
+
+a /= 2;
+
+cout << a << endl;
+
+double b = 2.458;
+
+cout << b << endl;
+
+float c = 34.2424;
+
+cout << c << endl;
+
+cout << pow(2, 3) << endl;
+
+cout << pow(a, 3) << endl;
+
+cout << sqrt(4) << endl;
+
+cout << round(2.6) << endl;
+
+cout << round(2.4) << endl;
+
+cout << ceil(2.1) << endl;
+
+cout << floor(3.9) << endl;
+
+cout << fmax(5,3) << endl;
+
+cout << fmin(4,10) << endl;
+
+cout << "Lets have some fun with stings !! \n";
+
+    string aero1 = "ISRO";
+
+    cout << aero1 << endl;
+     cout << aero1.length() << endl;
+      cout << aero1[0] << endl;
+       cout << aero1[1] << endl;
+        cout << aero1[2] << endl;
+         cout << aero1[3] << endl;
+          cout << aero1.find("R", 0) << endl;
+           cout << aero1.substr(0, 2) << endl;
+
+string aero3 = "spacex";
+
+cout << aero3 << endl;
+
+aero3[5] = 'e';
+
+cout << aero3 << endl;
+
+cout << aero3.find("c", 0) << endl;
+
+cout << aero3.substr(2, 5) << endl;
+
+
+int x;
+
+cout << "Enter your favourite number: ";
+cin >> x;
+
+cout << "ohhhhh!!! Your favourite num is " << x << " great! \n";
+
+char y;
+
+cout << "Enter your favourite english letter: ";
+cin >> y;
+
+cout << "ohhhh!!!! Your favourite english letter is '" << y << "' great! \n";
+
+cout << "Lets add two numbers !!!!!!!!!!!!!\n";
+
+int e, f;
+
+cout << "Enter the first number: ";
+cin >> e;
+
+cout << "Enter the second number: ";
+cin >> f;
+
+cout << "ANS: " << e + f << endl;
+
+int g[] = { 1, 2 , 2423, 24, 2424 } ;
+
+cout << g[2] << endl;
+
+g[2] = 7;
+
+cout << g[2] << endl;
+
+int h[100] = {1 , 34, 2};
+
+h[50] = 6;
+
+cout << h[50] << endl;
